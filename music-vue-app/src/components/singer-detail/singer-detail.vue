60c1f0331d86a366a2f940fb5722992325ee1771
lushan.fu <lushan.fu@pp.plateno.cc>
2017-11-28 17:20:22 +0800

    [歌曲列表和详情加载样式]

---
+++ music-vue-app/src/components/singer-detail/singer-detail.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+           歌手详情页面
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import { ERR_OK } from '../../api/dataConfig'
+
+  export default {
+    data () {
+      return {}
+    },
+    created () {
+    },
+    methods: {},
+    components: {}
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .slide-enter-active, .slide-leave-active
+    transition: all 0.3s
+
+  .slide-enter, .slide-leave-to
+    transform: translate3d(100%, 0, 0)
+</style>
--- music-vue-app/src/components/singer/singer.vue
+++ music-vue-app/src/components/singer/singer.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>
-    歌手页面
+  <div class="singer" ref="singer">
+    <list-view @select="selectSinger" :data="singerList" ref="list"></list-view>
+    <root-view></root-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import { ERR_OK } from '../../api/dataConfig'
   import { getSingerList } from '../../api/singer'
+  import Singer from '../../common/js/singer'
+  import ListView from '../../base/listview/listview'
+
+  const HOT_SINGER_LEN = 10
+  const HOT_NAME = '热门'
 
   export default {
     data () {
@@ -21,15 +27,70 @@
       _getSingerList () {
         getSingerList().then(res => {
           if (res.code === ERR_OK) {
-            this.singerList = res.data.list
+            this.singerList = this._normalizeSinger(res.data.list)
+            console.log('singer', this.singerList)
           }
         })
-      }
+      },
+      _normalizeSinger (list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index <= HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            }))
+          }
+
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        })
+        // 为了得到有序列表，我们需要处理 map
+        let ret = []
+        let hot = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
+      },
+      selectSinger(singer) {
+        this.$router.push({
+          path: `/singer/${singer.id}`
+        })
+      },
     },
-    components: {}
+    components: {
+      ListView
+    }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
